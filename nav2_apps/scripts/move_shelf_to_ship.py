043aeb19f0711f740cf4f00a5159f8bb105b188f
Miguel Solis Segura <miguelsolissegura@outlook.com>
2024-04-16 20:52:03 +0000

    Implement footprint adaptation

--- nav2_apps/scripts/move_shelf_to_ship.py
+++ nav2_apps/scripts/move_shelf_to_ship.py
@@ -6,6 +6,7 @@ from rclpy.duration import Duration
 import rclpy
 from rclpy.node import Node
 from attach_service.srv import GoToLoading
+from geometry_msgs.msg import Polygon, Point32
 
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 
@@ -20,6 +21,57 @@ class ClientAsync(Node):
     def send_request(self):
         req = GoToLoading.Request()
         self.future = self.client.call_async(req)
+
+class PolygonPublisher(Node):
+    def __init__(self):
+        super().__init__('polygon_publisher')
+        self.global_publisher_ = self.create_publisher(Polygon, '/global_costmap/footprint', 10)
+        self.local_publisher_ = self.create_publisher(Polygon, '/local_costmap/footprint', 10)
+        self.timer = self.create_timer(1, self.publish_polygon)
+
+        self.square_size = 0.9
+        self.half_size = self.square_size / 2 
+
+        self.circle_shape = [
+            Point32(x=3.050741195678711, y=-1.0888712406158447, z=0.0),
+            Point32(x=3.066046714782715, y=-0.9825969934463501, z=0.0),
+            Point32(x=3.0402321815490723, y=-0.8885296583175659, z=0.0),
+            Point32(x=2.9803848266601562, y=-0.8115015625953674, z=0.0),
+            Point32(x=2.895615339279175, y=-0.7632395029067993, z=0.0),
+            Point32(x=2.7798526287078857, y=-0.7447766065597534, z=0.0),
+            Point32(x=2.6857852935791016, y=-0.770591139793396, z=0.0),
+            Point32(x=2.608757257461548, y=-0.8304387331008911, z=0.0),
+            Point32(x=2.560495138168335, y=-0.9152079820632935, z=0.0),
+            Point32(x=2.542032241821289, y=-1.030970811843872, z=0.0),
+            Point32(x=2.5678467750549316, y=-1.1250381469726562, z=0.0),
+            Point32(x=2.6276943683624268, y=-1.20206618309021, z=0.0),
+            Point32(x=2.712463617324829, y=-1.2503283023834229, z=0.0),
+            Point32(x=2.828226327896118, y=-1.2687911987304688, z=0.0),
+            Point32(x=2.9222936630249023, y=-1.2429766654968262, z=0.0),
+            Point32(x=2.999321937561035, y=-1.183129072189331, z=0.0),
+        ]
+
+        self.square_shape = [
+            Point32(x=-self.half_size, y=self.half_size, z=0.0), 
+            Point32(x=self.half_size, y=self.half_size, z=0.0),    
+            Point32(x=self.half_size, y=-self.half_size, z=0.0),   
+            Point32(x=-self.half_size, y=-self.half_size, z=0.0)   
+        ]
+
+    def publish_polygon(self, mode):
+        polygon_msg = Polygon()
+        if (mode == 'circle'):
+            polygon_msg.points = self.circle_shape
+        elif (mode == 'square'):
+            polygon_msg.points = self.square_shape
+        else:
+            self.get_logger().info(f'Invalid shape type {mode}')
+            return None
+        self.global_publisher_.publish(polygon_msg)
+        self.local_publisher_.publish(polygon_msg)
+        self.get_logger().info(f'Publishing polygon footprint of type {mode}')
+        return None
+
 
 # Shelf positions for picking
 shelf_positions = {
@@ -90,9 +142,11 @@ def main():
     if result == TaskResult.SUCCEEDED:
         print('Calling shelf lifting service.')
 
-        # Instance the service client
+        # Instance the service client for shelf lifting
         client = ClientAsync()
         client.send_request()
+        # Instance the footprint publisher
+        footprint_publisher = PolygonPublisher()
 
         while rclpy.ok():
             rclpy.spin_once(client)
@@ -104,6 +158,7 @@ def main():
                         'Service call failed %r' % (e,))
                 else:
                     client.get_logger().info(f'Result of service call: {response.complete}')
+                    footprint_publisher.publish_polygon('square')
                 break
 
         client.destroy_node()
